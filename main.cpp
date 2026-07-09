d755595b7429025d2e3b5871d5959631d1282152
julia-GDB <devlopoer@gmail.com>
2026-07-09 23:17:22 +0700

    set up zig config

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream> 
 
+
+extern "C" {
+  void connect(); 
+}
+
 int 
 main(void) {
-  std::cout << "Hello, World!" ;
+  std::cout << "Hello, World! from C++" ;
   return 0;
 }
---
+++ src/utils.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+export fn connect() void {
+    std.debug.print("Hello From Zig\n", .{}); 
+}
